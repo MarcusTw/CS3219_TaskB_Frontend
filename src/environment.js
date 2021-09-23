f5e8c8787ebd2e9ff35a25a68eb9acf75aa6a6ae
Marcus <e0014884@u.nus.edu>
2021-09-23 11:02:34 +0800

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,4 @@
-const PROD_API = "https://random-asian-user-api.herokuapp.com/api/users";
+const PROD_API = "https://rau-api.herokuapp.com/api/users";
 const DEFAULT_PICTURE = "https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg";
 const IMAGE_ONERROR = (ev) => {
     ev.target.onerror = null;
