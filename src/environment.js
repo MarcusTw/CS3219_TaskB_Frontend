a1fa7588d945b1a3614168d728ba01c178563259
Marcus <e0014884@u.nus.edu>
2021-09-23 16:45:34 +0800

    Update Prod API

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,4 @@
-const PROD_API = "https://random-asian-user-api.herokuapp.com/api/users";
+const PROD_API = "https://cs3219-taskb-m4jbzqtwiq-de.a.run.app/api/users";
 const DEFAULT_PICTURE = "https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg";
 const IMAGE_ONERROR = (ev) => {
     ev.target.onerror = null;
